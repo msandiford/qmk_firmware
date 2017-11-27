198e36c6a3df51893fdba026c2660733c43631f6
Martin Sandiford <ms@mcdev.com.au>
2017-11-27 22:38:43 +1030

    Set backight pin correctly for JD40.

--- keyboards/jd40/config.h
+++ keyboards/jd40/config.h
@@ -55,6 +55,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 //#define MATRIX_HAS_GHOST
 
 /* number of backlight levels */
+#define BACKLIGHT_PIN B7
 #define BACKLIGHT_LEVELS 3
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
