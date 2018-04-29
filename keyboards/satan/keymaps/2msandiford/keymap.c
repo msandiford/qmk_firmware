d3705bc9b6fd9c43401e6d509209dccd3b6b55a2
Martin Sandiford <ms@mcdev.com.au>
2018-04-29 19:44:56 +0930

    Change Caps Lock to layer key and add cursor keys on IJKL on the new layer.

--- keyboards/satan/keymaps/2msandiford/keymap.c
+++ keyboards/satan/keymaps/2msandiford/keymap.c
@@ -23,7 +23,8 @@ extern keymap_config_t keymap_config;
 
 enum gh60_layers {
   _QWERTY,
-  _OTHER
+  _OTHER1,
+  _OTHER2
 };
 
 enum planck_keycodes {
@@ -39,7 +40,8 @@ enum planck_keycodes {
 #define XXXXXXX KC_NO
 
 // Layer change
-#define OTHER           MO(_OTHER)              /* Momentary use of _OTHER layer */
+#define OTHER1          MO(_OTHER1)              /* Momentary use of _OTHER1 layer */
+#define OTHER2          MO(_OTHER2)              /* Momentary use of _OTHER2 layer */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* _QWERTY: Qwerty Layer
@@ -58,12 +60,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = {
     { KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC },
     { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS },
-    { KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT  },
+    { OTHER2,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, XXXXXXX, KC_ENT  },
     { KC_LSFT, XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX, KC_RSFT },
-    { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, OTHER,   KC_APP,  KC_RCTL }
+    { KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_RALT, OTHER1,  KC_APP,  KC_RCTL }
   },
 
-  /* _OTHER: Function Layer
+  /* _OTHER1: Function Layer
    * ,-----------------------------------------------------------------------------------------------------------------------.
    * |   `   |  F1   |  F2   |  F3   |  F4   |  F5   |  F6   |  F7   |  F8   |  F9   |  F10  |  F11  |  F12  |      Del      |
    * |-----------------------------------------------------------------------------------------------------------------------|
@@ -76,12 +78,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |         |         |         |                     MS Btn 2                    |         |         |         |         |
    * `-----------------------------------------------------------------------------------------------------------------------'
    */
-  [_OTHER] = {
+  [_OTHER1] = {
     { KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL  },
     { AG_TOGG, KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, KC_INS,  BL_TOGG, MMV_UL,  KC_MS_U, MMV_UR,  XXXXXXX, KC_PSCR, KC_SLCK, KC_PAUS },
-    { _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_DEL,  BL_INC,  KC_MS_L, KC_BTN1, KC_MS_R, XXXXXXX, XXXXXXX, XXXXXXX, _______ },
+    { KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  KC_DEL,  BL_INC,  KC_MS_L, KC_BTN1, KC_MS_R, XXXXXXX, XXXXXXX, XXXXXXX, _______ },
     { _______, XXXXXXX, KC_MPLY, KC_MUTE, KC_MNXT, KC_VOLD, KC_VOLU, BL_DEC,  MMV_DL,  KC_MS_D, MMV_DR,  XXXXXXX, XXXXXXX, _______ },
     { _______, _______, _______, _______, _______, KC_BTN2, _______, _______, _______, _______, _______, _______, _______, _______ }
+  },
+
+  /* _OTHER2: Function Layer
+   * ,-----------------------------------------------------------------------------------------------------------------------.
+   * |   `   |  F1   |  F2   |  F3   |  F4   |  F5   |  F6   |  F7   |  F8   |  F9   |  F10  |  F11  |  F12  |      Del      |
+   * |-----------------------------------------------------------------------------------------------------------------------|
+   * | AG Toggle |       |       |       |       |  Ins  |BL Togg| Pg Up |   Up  | Pg Dn | Home  | PrtSc | ScLck |   Pause   |
+   * |-----------------------------------------------------------------------------------------------------------------------|
+   * |             |       |       |       |       |  Del  |BL  Inc| Left  | Down  | Right |  End  |       |                 |
+   * |-----------------------------------------------------------------------------------------------------------------------|
+   * |                 |Ply/Pse| Mute  | Next  | Vol-  | Vol+  |BL  Dec|       |       |       |       |                     |
+   * |-----------------------------------------------------------------------------------------------------------------------|
+   * |         |         |         |                     MS Btn 2                    |         |  Caps   |         |         |
+   * `-----------------------------------------------------------------------------------------------------------------------'
+   */
+  [_OTHER2] = {
+    { KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL  },
+    { AG_TOGG, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_INS,  BL_TOGG, KC_PGUP, KC_UP,   KC_PGDN, KC_HOME, KC_PSCR, KC_SLCK, KC_PAUS },
+    { OTHER2,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_DEL,  BL_INC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  XXXXXXX, XXXXXXX, _______ },
+    { _______, XXXXXXX, KC_MPLY, KC_MUTE, KC_MNXT, KC_VOLD, KC_VOLU, BL_DEC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______ },
+    { _______, _______, _______, _______, _______, KC_BTN2, _______, _______, _______, _______, _______, KC_CAPS, _______, _______ }
   }
 };
 
