750c765e4eb087b8e1bea8841c037d700add4b3a
Martin Sandiford <ms@mcdev.com.au>
2019-12-01 11:35:13 +0000

    Change manufacturer and product names for YD60MQ

---
+++ keyboards/yd60mq/keymaps/2msandiford/config.h
@@ -0,0 +1,14 @@
+#ifndef CONFIG_USER_H
+#define CONFIG_USER_H
+#include "../../config.h"
+
+//#define TAPPING_TERM 25
+//#define RETRO_TAPPING
+//#define PERMISSIVE_HOLD
+
+#undef MANUFACTURER
+#define MANUFACTURER    MCCORP
+#undef PRODUCT
+#define PRODUCT         MSandiford
+
+#endif
